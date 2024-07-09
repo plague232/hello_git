2ecf3421cf4727b6ce1c872fa6ead86d332ffc66
plague232 <vermarohit1225@gmail.com>
2024-07-09 11:27:08 +0530

    first factorial code

---
+++ factorial.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+	int n;
+	cin>>n;
+	
+	int fact;
+	
+	if(n<0){
+	    cout<<"error";
+	    
+	}
+	else if(n=0){
+	    cout<<1;
+	}
+	else{
+ 	    fact=
+		
+		
+
+
+
+
+
+	
+	
+	return 0;
+}
